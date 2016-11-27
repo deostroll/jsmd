5e9c4816401cc058b8bf4f7b5ad286782a7bf20a
deostroll <deostroll@gmail.com>
2016-11-27 23:53:42 +0530

    fixed discrepancy due to promise

--- tests/files/parsing.js
+++ tests/files/parsing.js
@@ -2,44 +2,67 @@ var expect = chai.expect;
 var assert = chai.assert;
 
 var load = function(url) {
-  return new Promise(function(done){
-    $.ajax({
-      url: url,
-      dataType: 'json',
-      success: done
-    })
+  // return new Promise(function(done, fail){
+  //   $.ajax({
+  //     url: url,
+  //     dataType: 'json',
+  //     success: done,
+  //     error: fail
+  //   });
+  // });
+  return $.ajax({
+    url: url,
+    dataType: 'json'
   });
 };
 
 describe('parsing tests', function(){
   it('should parse data2.json file correctly', function(done){
-    $.ajax({
-      url: 'data2.json',
-      dataType: 'json',
-      success: function(data) {
-        // console.log(data.type);
-        var model = ModelBuilder.walk(data);
-        expect(model).to.not.be.undefined;
-        expect(model.name).to.equal('model');
-        expect(model.entities.length).to.equal(1);
-        var entity = model.entities[0];
-        expect(entity.name).to.equal('customer');
-        expect(entity.fields.length).to.equal(1);
-        expect(entity.fields[0].name).to.equal('customerId');
-        // console.log(JSON.stringify(model, null, 2));
-        done();
-      }
-    })
+    // $.ajax({
+    //   url: 'data2.json',
+    //   dataType: 'json',
+    //   success: function(data) {
+    //     console.log(data.type);
+    //     var model = ModelBuilder.walk(data);
+    //     expect(model).to.not.be.undefined;
+    //     expect(model.name).to.equal('model');
+    //     expect(model.entities.length).to.equal(1);
+    //     var entity = model.entities[0];
+    //     expect(entity.name).to.equal('customer');
+    //     expect(entity.fields.length).to.equal(1);
+    //     expect(entity.fields[0].name).to.equal('customerId');
+    //     // console.log(JSON.stringify(model, null, 2));
+    //     done();
+    //   }
+    // });
+    load('data2.json').then(function(data){
+      expect(data).to.not.be.undefined;
+      // console.log(data.type);
+      var model = ModelBuilder.walk(data);
+      expect(model).to.not.be.undefined;
+      expect(model.name).to.equal('model');
+      expect(model.entities.length).to.equal(1);
+      var entity = model.entities[0];
+      expect(entity.name).to.equal('customer');
+      expect(entity.fields.length).to.equal(1);
+      expect(entity.fields[0].name).to.equal('customerId');
+      // console.log(JSON.stringify(model, null, 2));
+      done();
+
+    }, done);
   });
 
   it('should parse sample3 correctly', function(done){
     load('sample3.json')
     .then(function(ast){
+      // console.log(ast.type);
       var expected = 'northWind';
       var model = ModelBuilder.walk(ast);
       expect(model.name).to.equal(expected);
-      console.log(model.name);
+      // console.log(model.name);
       done();
+    }, function(){
+      chai.fail();
     });
-  })
+  });
 });
