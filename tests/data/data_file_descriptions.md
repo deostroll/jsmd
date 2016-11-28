62f44e33aca47937a0e8ab2da63e10d921a509c8
deostroll <deostroll@gmail.com>
2016-11-28 11:50:20 +0530

    committed sample5

--- tests/data/data_file_descriptions.md
+++ tests/data/data_file_descriptions.md
@@ -2,3 +2,4 @@
  - **data2.json** : a model with customer entity with one field
  - **sample3.js** and **sample3.json** : same as the above, but with a different model name
  - **sample4.js** and **sample4.json** : model with 2 entities
+ - **sample5.js** and **sample5.json** : model with 1 entity and a primary key
---
+++ tests/data/sample5.js
@@ -0,0 +1,23 @@
+var student = {
+  studentId: {
+    definition : {
+      type: 'string',
+      size: 5
+    }
+  },
+  firstName: {
+    definition: {
+      type: 'string',
+      size: 15
+    }
+  },
+  lastName: {
+    defition: {
+      type: 'string',
+      size: 15
+    }
+  },
+  '$key': [
+    'studentId'
+  ]
+};
